7b03c619a7c833254f463bb4bb0f2b303da48d14
hulahoops123 <jdmichael.giveitback@gmail.com>
2022-07-22 11:52:53 +0200

    installed and configured tailwind

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import global from './global'
+import './index.css'
 const app =createApp(App);
 app.use(router);
 app.provide('global',{global})   //expects 2 arguments??
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,4 +1,5 @@
 <template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
   <button v-if="!isNewUser" @click="googleSignIn">googleIn</button>
   <div v-if="isNewUser">
     <UsernameComponent
